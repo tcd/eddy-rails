e99917174bc5fb5cc8d8a5ccdf311c1745cc7183
Clay Dunston <claydunston@gmail.com>
2020-01-05 13:21:17 -0600

    Remove unused code in `app/`

--- app/controllers/eddy/rails/application_controller.rb
+++ app/controllers/eddy/rails/application_controller.rb
@@ -1,7 +1,6 @@
 module Eddy
   module Rails
     class ApplicationController < ActionController::API
-      # protect_from_forgery with: :exception
     end
   end
 end
--- app/jobs/eddy/rails/application_job.rb
+++
@@ -1,6 +0,0 @@
-module Eddy
-  module Rails
-    class ApplicationJob < ActiveJob::Base
-    end
-  end
-end
