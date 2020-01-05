22afbe2d7a556f21bced943c522005e09207f2d2
Clay Dunston <claydunston@gmail.com>
2020-01-05 13:42:45 -0600

    Update test

--- test/eddy/rails_test.rb
+++ test/eddy/rails_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class Eddy::Rails::Test < ActiveSupport::TestCase
-  test "truth" do
+
+  test "that 'Eddy::Rails' is exists" do
     assert_kind_of(Module, Eddy::Rails)
   end
+
+  test "that 'Eddy::Rails' has a version" do
+    refute_nil(Eddy::Rails::VERSION)
+  end
+
 end
